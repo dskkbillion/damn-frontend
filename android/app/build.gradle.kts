f915b9cdc5661454343f302bea1cf20681237b71
Paper-blue <2311073425@qq.com>
2025-04-02 22:15:01 -0500

    Feature: Implement basic voice message recording and playback UI (Mock env)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,13 +11,12 @@ android {
     ndkVersion = "27.0.12077973"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 
     kotlinOptions {
-        // Align Kotlin JVM target with Java compatibility
-        jvmTarget = JavaVersion.VERSION_1_8.toString()
+        jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
     defaultConfig {
@@ -25,7 +24,7 @@ android {
         applicationId = "com.example.dskk_flutter_refactor"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
