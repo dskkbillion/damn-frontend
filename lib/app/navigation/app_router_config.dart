a8328d2a667485065468637d419fcd5a98f65aae
scallioncake <scallioncakerui@gmail.com>
2025-08-27 01:45:35 +0800

    fix: 修改开发tab显示设置为false
    - 将_app_router_config.dart中的_showDevTab属性从true更改为false，以禁用开发tab的显示。
    
    此更改旨在优化应用的用户界面，确保用户体验更加简洁。

--- lib/app/navigation/app_router_config.dart
+++ lib/app/navigation/app_router_config.dart
@@ -3,7 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 /// 应用路由配置类，用于控制路由行为
 class AppRouterConfig {
   /// 是否显示开发tab
-  static bool _showDevTab = true;
+  static bool _showDevTab = false;
 
   /// 设置是否显示开发tab
   static void setShowDevTab(bool show) {
